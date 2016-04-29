856c49b4bfb8d9bedc04008c009cc2a9eda902b3
Ryan Kliemann <Ryan_blake@icloud.com>
2016-04-29 12:38:40 -0500

    styled n cleaned waypoints

--- scripts/waypoint.js
+++ scripts/waypoint.js
@@ -1,24 +1,44 @@
 
 $(document).ready(function() {
-var waypoints = $('#second').waypoint({
-  handler: function(direction) {
-    // alert(this.element.id + ' hit')
-  },
-  offset: '25%'
+// box and inside text below header fade out left
+var waypoint4 = $('.center').waypoint({
+    handler: function(direction) {
+      if (direction === 'down') {
+        $('.center').addClass('animated fadeOutLeftBig');
+      }
+      //  remove else to allow class to stay on page
+      else {
+        $('.center').removeClass('animated fadeOutLeftBig');
+      }
+    },
+    offset: '18%'
+  })
+  var waypoint4 = $('.diamond').waypoint({
+    handler: function(direction) {
+      if (direction === 'down') {
+        $('.diamond').addClass('animated fadeOutLeftBig');
+      }
+      //  remove else to allow class to stay on page
+      else {
+        $('.diamond').removeClass('animated fadeOutLeftBig');
+      }
+    },
+    offset: '20%'
 })
 
+// first block image fadeIn
 var waypoint2 = $('.top').waypoint({
   handler: function(direction) {
     if (direction === 'down') {
-      $('.top').addClass('animated fadeIn');
+      $('.top').addClass('animated flip');
     }
     else {
-      $('.top').removeClass('animated fadeIn');
+      $('.top').removeClass('animated flip');
     }
   },
   offset: '40%'
 })
-
+// second block image fadeInUp and text inside
   $('.bottom').css('opacity', 0);
 
 var waypoint3 = $('.bottom').waypoint({
@@ -31,8 +51,21 @@ var waypoint3 = $('.bottom').waypoint({
     //   $('.bottom').removeClass('animated fadeInUp');
     // }
   },
-  offset: '20%'
+  offset: '25%'
 })
 
+$('.bottomText').css('opacity', 0);
+var waypoint4 = $('.bottomText').waypoint({
+  handler: function(direction) {
+    if (direction === 'down') {
+      $('.bottomText').addClass('animated fadeIn');
+    }
+    //  remove else to allow class to stay on page
+    // else {
+    //   $('.bottom').removeClass('animated fadeInUp');
+    // }
+  },
+  offset: '48%'
+})
 
 });
