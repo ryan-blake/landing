f692d4ecb6009bf79a67e3ea181a2715d53d143c
Ryan Kliemann <Ryan_blake@icloud.com>
2016-05-06 05:24:21 -0500

    bootstrapped

--- scripts/home.js
+++ scripts/home.js
@@ -5,13 +5,22 @@ var revealPoint = function() {
   navbar.style.WebkitTransform = "scaleX(1) translateY(0)";
 
 }
-var center = document.getElementsByClassName('diamond');
 
+var center = document.getElementsByClassName('diamond');
+var dText = document.getElementsByClassName('d3');
+var dHead = document.getElementsByClassName('d2');
 $(document).ready(function() {
 $( center ).click(function( event ) {
-    event.preventDefault();
-    $( this ).hide( "slow" );
+    $( dText ).toggle('slow');
 });
+
+//cursor: pointer
+// $(document).ready(function() {
+// $( center ).click(function( event ) {
+//     ß$( this ).hide('slow');
+    // display: none && visibilty: none
+    // different *z-index
+// });
 // keep full size background after click.
 
 $(window).scroll(function() {
